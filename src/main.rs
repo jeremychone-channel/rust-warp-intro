64e9ae7930aa79cdb31b2dfa077cdad94d9ae80c
Jeremy Chone <jeremy.chone@gmail.com>
2021-11-02 09:25:41 -0700

    015

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use warp::Filter;
 async fn main() {
 	let hello_world = warp::path::end().map(|| "Hello world from root!");
 
+	let hi = warp::path("hi").map(|| "Hello from hi");
+
+	let routes = hello_world.or(hi);
+
 	println!("start web-server");
-	warp::serve(hello_world).run(([127, 0, 0, 1], 8080)).await;
+	warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
