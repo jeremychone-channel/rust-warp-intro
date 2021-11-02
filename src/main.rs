8de01693ad3ddaa5da8d4701988359403f3bf463
Jeremy Chone <jeremy.chone@gmail.com>
2021-11-02 09:39:42 -0700

    019

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ const WEB_FOLDER: &str = "web-folder/";
 
 #[tokio::main]
 async fn main() {
-	let hello_world = warp::path::end()
-		.and(warp::get())
-		.map(|| "Hello world from root!");
-
+	// APIs
 	let hi = warp::path("hi").and(warp::get()).map(|| "Hello from hi");
+	let apis = hi;
 
+	// Static Content
 	let content = warp::fs::dir(WEB_FOLDER);
+	let root = warp::get()
+		.and(warp::path::end())
+		.and(warp::fs::file(format!("{}/index.html", WEB_FOLDER)));
+	let static_site = content.or(root);
 
-	let routes = hello_world.or(hi).or(content);
+	let routes = apis.or(static_site);
 
 	println!("start web-server");
 	warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
