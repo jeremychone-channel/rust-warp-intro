8c678e8c57dcdc7daa3f836f1dcfbb76e6e2efe0
Jeremy Chone <jeremy.chone@gmail.com>
2021-11-02 09:13:48 -0700

    013

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-	println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+	let hello_world = warp::path::end().map(|| "Hello world from root!");
+
+	println!("start web-server");
+	warp::serve(hello_world).run(([127, 0, 0, 1], 8080)).await;
 }
