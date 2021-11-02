2163a28d2097c2c76b7c735b677cebe03a31ec82
Jeremy Chone <jeremy.chone@gmail.com>
2021-11-02 11:19:08 -0700

    029

--- src/security.rs
+++ src/security.rs
@@ -2,8 +2,8 @@ use warp::Filter;
 
 const HEADER_XAUTH: &str = "X-Auth-Token";
 
-pub fn check_auth(
-) -> impl Filter<Extract = ((),), Error = warp::Rejection> + Clone {
+pub fn do_auth(
+) -> impl Filter<Extract = (UserCtx,), Error = warp::Rejection> + Clone {
 	warp::any()
 		.and(warp::header::<String>(HEADER_XAUTH))
 		.and_then(|xauth: String| async move {
@@ -12,8 +12,20 @@ pub fn check_auth(
 				return Err(warp::reject::custom(FailAuth));
 			}
 
-			Ok::<(), warp::Rejection>(())
+			if let Some(user_id) = xauth
+				.splitn(2, ".")
+				.next()
+				.and_then(|v| v.parse::<i64>().ok())
+			{
+				Ok::<UserCtx, warp::Rejection>(UserCtx { user_id })
+			} else {
+				Err(warp::reject::custom(FailAuth))
+			}
 		})
+}
+
+pub struct UserCtx {
+	pub user_id: i64,
 }
 
 #[derive(Debug)]
--- src/todo_rest.rs
+++ src/todo_rest.rs
@@ -1,4 +1,4 @@
-use crate::security::check_auth;
+use crate::security::{do_auth, UserCtx};
 use serde_json::{json, Value};
 use warp::{reply::Json, Filter};
 
@@ -9,25 +9,25 @@ pub fn todos_filter(
 	let list = todos_base
 		.and(warp::get())
 		.and(warp::path::end())
-		.and(check_auth().untuple_one())
+		.and(do_auth())
 		.and_then(todo_list);
 
 	let get = todos_base
 		.and(warp::get())
-		.and(check_auth().untuple_one())
+		.and(do_auth())
 		.and(warp::path::param()) // e.g., /todos/123
 		.and_then(todo_get);
 
 	let create = todos_base
 		.and(warp::post())
-		.and(check_auth().untuple_one())
+		.and(do_auth())
 		.and(warp::body::json())
 		.and_then(todo_create);
 
 	list.or(get).or(create)
 }
 
-async fn todo_list() -> Result<Json, warp::Rejection> {
+async fn todo_list(_user_ctx: UserCtx) -> Result<Json, warp::Rejection> {
 	// TODO - get from DB
 	let todos = json!([
 		{"id": 1, "title": "todo 1"},
@@ -39,10 +39,13 @@ async fn todo_list() -> Result<Json, warp::Rejection> {
 	Ok(todos)
 }
 
-async fn todo_get(id: i64) -> Result<Json, warp::Rejection> {
+async fn todo_get(
+	_user_ctx: UserCtx,
+	id: i64,
+) -> Result<Json, warp::Rejection> {
 	// TODO - get from DB
 	let todo = json!(
-		{"id": id, "title": format!("todo {}", id)}
+		{"id": id, "user_id": _user_ctx.user_id, "title": format!("todo {}", id)}
 	);
 
 	// serde-json to warp-reply
@@ -51,7 +54,10 @@ async fn todo_get(id: i64) -> Result<Json, warp::Rejection> {
 	Ok(todo)
 }
 
-async fn todo_create(data: Value) -> Result<Json, warp::Rejection> {
+async fn todo_create(
+	_user_ctx: UserCtx,
+	data: Value,
+) -> Result<Json, warp::Rejection> {
 	// TODO - write to DB
 	let todo = data;
 
