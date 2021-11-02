0f7eca4f68de61e687bf5c602d37ac3dfe2c1e63
Jeremy Chone <jeremy.chone@gmail.com>
2021-11-02 10:25:04 -0700

    023

--- src/todo_rest.rs
+++ src/todo_rest.rs
@@ -1,4 +1,5 @@
-use warp::Filter;
+use serde_json::json;
+use warp::{reply::Json, Filter};
 
 pub fn todos_filter(
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -9,6 +10,14 @@ pub fn todos_filter(
 		.and_then(todo_list)
 }
 
-async fn todo_list() -> Result<String, warp::Rejection> {
-	Ok("Will list todos".to_string())
+async fn todo_list() -> Result<Json, warp::Rejection> {
+	// TODO - get from DB
+	let todos = json!([
+		{"id": 1, "title": "todo 1"},
+		{"id": 2, "title": "todo 2"}
+	]);
+
+	let todos = warp::reply::json(&todos);
+
+	Ok(todos)
 }
